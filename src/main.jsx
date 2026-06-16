f0468d165e6c297ed0cd8ac83433e94371036fa6
Fayaz Ahamed <151362612+fayazahamed12@users.noreply.github.com>
2026-06-16 19:36:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import GlobalState from "./Context";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const GOOGLE_CLIENT_ID = "216863211385-an6n48bg5d1gasu6rdkgsb6mm0akcbbv.apps.googleusercontent.com"; // Replace with your actual Google Client ID
+const GOOGLE_CLIENT_ID = "993408333636-a6j5pa2ron46tjlpmigio012859rg7i7.apps.googleusercontent.com"; // Replace with your actual Google Client ID
 
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
